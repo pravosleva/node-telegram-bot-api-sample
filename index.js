1bf2bf8341c3bccb02a0fa141005bb671be8e65d
Den <selection4test@gmail.com>
2021-07-10 17:21:58 +0300

    fix

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.onText(/\/wtf (.+)/, function (msg, match) {
 
   // const res = await axios.get(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage?text=${msg}&chat_id=${DEVELOPER_CHAT_ID}`)
 
-  bot.sendMessage(Number(DEVELOPER_CHAT_ID), `New Entry from @${msg.chat.username}):` + '\n\n' + message)
+  bot.sendMessage(Number(DEVELOPER_CHAT_ID), `New Entry from @${msg.chat.username}:` + '\n\n' + message)
   bot.sendMessage(senderChatId, `Ok ${msg.chat.first_name}, your msg sent to ${DEVELOPER_NAME}`)
 })
 
