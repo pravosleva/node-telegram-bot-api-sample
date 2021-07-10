5798d292c8ac9abd1db59be6e62c63acbde0913f
Den <selection4test@gmail.com>
2021-07-10 16:52:31 +0300

    /wtf msg for developer added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 const path = require('path')
+// const axios = require('axios')
 
 require('dotenv').config({ path: path.join(__dirname, './.prod.env') })
 
@@ -8,10 +9,14 @@ const gksLogic = require('./contragents/gcs/logic')
 // Others...
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TG_BOT_TOKEN
+const {
+  TG_BOT_TOKEN,
+  DEVELOPER_NAME,
+  DEVELOPER_CHAT_ID,
+} = process.env
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true })
+const bot = new TelegramBot(TG_BOT_TOKEN, { polling: true })
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, function (msg, match) {
@@ -25,6 +30,17 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp)
+})
+
+// Matches "/wtf [whatever]"
+bot.onText(/\/wtf (.+)/, async function (msg, match) {
+  const senderChatId = msg.chat.id
+  const msg = match[1] // the captured "whatever"
+
+  // const res = await axios.get(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage?text=${msg}&chat_id=${DEVELOPER_CHAT_ID}`)
+
+  bot.sendMessage(DEVELOPER_CHAT_ID, `BOT MSG: From ${senderChatId}` + '\n' + msg)
+  bot.sendMessage(senderChatId, `Ok, your msg sent to ${DEVELOPER_NAME}`)
 })
 
 gksLogic(bot)
