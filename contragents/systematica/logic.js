250429effacf27d18b59571d5beae12d6836f29f
Den <selection4test@gmail.com>
2021-08-11 15:40:26 +0300

    systematica completed

---
+++ contragents/systematica/logic.js
@@ -0,0 +1,63 @@
+const data = require('./data.json')
+
+function abSort(a, b) {
+  return a.localeCompare(b);
+}
+
+module.exports = (bot) => {
+  // Handle callback queries
+  bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+    const action = callbackQuery.data;
+    const msg = callbackQuery.message;
+    const userOpts = {
+      chat_id: msg.chat.id,
+      message_id: msg.message_id,
+    };
+    let text = 'Sorry, in progress...';
+    let options
+    switch(action) {
+      case 'systematica':
+        text = 'Выберите материалы Systematica';
+        options = {
+          reply_markup: JSON.stringify({
+            inline_keyboard: [
+              [{ text: 'Info', callback_data: 'systematica.info' }],
+              [{ text: 'Logo', callback_data: 'systematica.logo' }],
+              [{ text: 'Blanks', callback_data: 'systematica.blanks' }],
+              [{ text: 'Profiles', callback_data: 'systematica.profiles' }],
+              [{ text: 'Presentations', callback_data: 'systematica.presentations' }],
+              // [{ text: 'ДКСБ', callback_data: 'gcs.dcsb' }],
+            ]
+          })
+        };
+        bot.sendMessage(msg.chat.id, text, options);
+        return
+
+      // 1. Info
+      case 'systematica.info':
+        text = data.systematica.info.sort(abSort).join('\n');
+        bot.sendMessage(msg.chat.id, text);
+        return
+      case 'systematica.logo':
+        text = data.systematica.logo.sort(abSort).join('\n');
+        bot.sendMessage(msg.chat.id, text);
+        return
+      case 'systematica.blanks':
+        text = data.systematica.blanks.sort(abSort).join('\n');
+        bot.sendMessage(msg.chat.id, text);
+        return
+      case 'systematica.profiles':
+        text = data.systematica.profiles.sort(abSort).join('\n');
+        bot.sendMessage(msg.chat.id, text);
+        return
+      case 'systematica.presentations':
+        text = data.systematica.presentations.sort(abSort).join('\n');
+        bot.sendMessage(msg.chat.id, text);
+        return
+
+      default:
+        bot.editMessageText(text, userOpts)
+        return
+    }
+  });
+}
