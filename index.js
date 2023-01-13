aaa26d72f481ddb1223aa33e39a11cdb46b3832e
Den <selection4test@gmail.com>
2023-01-13 18:31:06 +0300

    fix

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ bot.onText(/(\/menu|Menu|\/baza|Baza|gcs)/, function(msg) {
   const options = {
     reply_markup: JSON.stringify({
       inline_keyboard: [
-        [{ text: '[K]abbalah[E]xpedient[R]use[N]everAgain', callback_data: 'test' }],
+        [{ text: '[K]abbalah [E]xpedient [R]use [N]everAgain', callback_data: 'test' }],
         // [{ text: 'ГКС', callback_data: 'gcs' }],
         // [{ text: 'Систематика', callback_data: 'systematica' }, { text: 'STEP LOGIC', callback_data: 'step_logic' }],
         // [{ text: 'HaedPoint', callback_data: 'haed_point' }, { text: 'Landata', callback_data: 'landata' }],
