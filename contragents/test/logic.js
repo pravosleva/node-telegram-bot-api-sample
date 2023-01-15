ee8614ad23c4176b51927b26421fe77bf0ba4bb9
Den <selection4test@gmail.com>
2023-01-15 03:50:55 +0300

    fix

--- contragents/test/logic.js
+++ contragents/test/logic.js
@@ -86,12 +86,13 @@ module.exports = (bot) => {
 
           if (strs.length > 0) {
             bot.sendMessage(msg.chat.id, `\`\`\`\n---\nTOTAL: ${items.length}\nLAST: ${getName(items[0])}\nWHEN: ${getTimeAgo(new Date(items[0].ts))}\n---\`\`\``, { parse_mode: "Markdown" });
-            strs.forEach(async s => {
-              await delay(1000)
-                .then(() => {
-                  bot.sendMessage(msg.chat.id, `\`${s}\``, { parse_mode: "Markdown" });
-                })
-            })
+            async function send() {
+              strs.forEach(async s => {
+                bot.sendMessage(msg.chat.id, `\`${s}\``, { parse_mode: "Markdown" });
+                await delay(1000)
+              })
+            }
+            send()
           } else throw new Error('Empty array')
         } catch (err) {
           text = err.message || 'ERR2: No err.message'
